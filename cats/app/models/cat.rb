31dbdef435bb62a08e72fb84de3d1a2be587ffe2
kat-onyx <sarah.kathryn.peters@gmail.com>
2018-11-13 17:05:34 -0800

    Add associations between cat and catRentalRequest and validate

--- cats/app/models/cat.rb
+++ cats/app/models/cat.rb
@@ -1,7 +1,27 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint(8)        not null, primary key
+#  birth_date  :string           not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Cat < ApplicationRecord
 
   validates :birth_date, :color, :name, :description, presence: true
   validate :sex
+
+
+  has_many :cat_rental_requests,
+    foreign_key: :cat_id,
+    class_name: :CatRentalRequest
+
 
   def sex_validation
     if sex.length > 1
@@ -9,5 +29,5 @@ class Cat < ApplicationRecord
     elsif :sex != 'M' || :sex != 'F'
       errors[:sex] << "Must be either M or F"
     end
-  end 
+  end
 end
